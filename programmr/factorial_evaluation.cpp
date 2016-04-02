7c0de036ac5672b8fb3635a8cae8b0065da9604a
Kibur <manu2293@hotmail.com>
2016-04-02 15:27:35 +0200

    Factorial Evaluation (Recursive function)

---
+++ programmr/factorial_evaluation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+
+long factorial(const long &n) {
+    for (long i = n; i > 1; i--) return n * factorial(i - 1);
+}
+
+int main() {
+    std::string strInput;
+    long n;
+
+    cout << "Enter the value of n: ";
+    getline(cin, strInput);
+    std::stringstream(strInput) >> n;
+
+    cout << "\nValue of factorial is: " << factorial(n);
+}
